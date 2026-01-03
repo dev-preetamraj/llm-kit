b2f4595c2fccd505513b1e7327b55939beeefb72
Preetam Raj <iitianpreetam@gmail.com>
2026-01-03 19:27:28 +0530

    added setup for doc

---
+++ docs/source/conf.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("../.."))
+
+project = "llm-kit-pro"
+copyright = "2024, Preetam Raj"
+author = "Preetam Raj"
+release = "0.2.3"
+
+# -- General configuration ---------------------------------------------------
+
+extensions = [
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    "sphinx.ext.viewcode",
+    "myst_parser",
+]
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = "sphinx_rtd_theme"
+html_static_path = ["_static"]
+
+# -- Myst Parser configuration -----------------------------------------------
+myst_enable_extensions = [
+    "amsmath",
+    "colon_fence",
+    "deflist",
+    "dollarmath",
+    "fieldlist",
+    "html_admonition",
+    "html_image",
+    "linkify",
+    "replacements",
+    "smartquotes",
+    "strikethrough",
+    "substitution",
+    "tasklist",
+]
---
+++ docs/source/index.rst
@@ -0,0 +1,22 @@
+.. llm-kit-pro documentation master file, created by
+   sphinx-quickstart on Sat Jan  4 2026.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+llm-kit-pro
+===========
+
+.. include:: ../../README.md
+   :parser: myst_parser.sphinx_
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
+
