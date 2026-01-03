61e8174d8e4bc9d67147664d5d6e86218af5510e
Preetam Raj <iitianpreetam@gmail.com>
2026-01-03 19:37:15 +0530

    updated requirements

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,7 +6,7 @@ sys.path.insert(0, os.path.abspath("../.."))
 project = "llm-kit-pro"
 copyright = "2024, Preetam Raj"
 author = "Preetam Raj"
-release = "0.2.3"
+release = "0.2.5"
 
 # -- General configuration ---------------------------------------------------
 
